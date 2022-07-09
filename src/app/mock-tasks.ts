7f510015a98f1767470f33c64d6541e54ba601e4
Cristian <veracristian89@gmail.com>
2022-07-09 09:39:05 -0300

    se agrega mock de tasklist

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import{Task} from "./Task"
+export const TASKS = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: "Agosto 5 a las 17:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar bootstrap",
+        day: "Agosto 5 a las 19:00",
+        reminder: false
+    },
+    {
+        id: 2,
+        text: "Leer codigo limpio",
+        day: "Agosto 5 a las 21:00",
+        reminder: false
+    }
+]
+
